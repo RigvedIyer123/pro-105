089d269ee5fdb6d50e8decbf1d3f96c4f9f91f46
RigvedIyer123 <86088571+RigvedIyer123@users.noreply.github.com>
2022-02-18 17:59:02 +0530

    Add files via upload

---
+++ stddev.py
@@ -0,0 +1,31 @@
+import csv
+import math
+
+with open("data.csv" , newline = "") as f:
+    reader = csv.reader(f)
+    filedata = list(reader)
+
+data = filedata[0]
+def mean(data):
+    n = len(data)
+    sum = 0
+    for x in data:
+        sum += int(x)
+    mean = sum/n
+    return mean
+
+squaredList = []
+for n in data:
+    a = int(n)-mean(data)
+    a = a**2
+    squaredList.append(a)
+
+sum = 0
+for i in squaredList:
+    sum = sum+i
+
+result = sum/(len(data)-1)
+
+stdev = math.sqrt(result)
+
+print(stdev)
